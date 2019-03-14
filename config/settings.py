c90417ba8bbd99d285ef0a9eea31d1c8f69a57e4
gurumee92 <32067275+gurumee92@users.noreply.github.com>
2019-03-15 00:33:18 +0900

    feature/TOOL_API #3 django-rest-framework 적용
    1. install djangorestframework
        - 1. pip install djangorestframework
        - 2. config/settings.py
    
              INSTALLED_APPS = [
                  ...,
                  "rest_framework",
                  ...
              ]
    
    2. tools/serializers.py 생성
        - 1. ToolsSerializer 생성 ModelSerializer 상속 받으면 모델을 쉽게 JSON화 하기 위한 Serializer가 쉽게 만들어진다.
    
    3. tools/views.py 수정
        - 1. 도구 목록에 대한 API 제공 ToolList(generic.ListCreateAPIView)
        - 2. 도구 하나에 대한 API 제공 Tool(generic.RetrieveUpdateDestroyAPIView)
    
    4. tools/urls.py 생성
    
    from django.urls import path
    from rest_framework.urlpatterns import format_suffix_patterns
    
    from .views import *
    
    urlpatterns = [
        path('tools/', ToolList.as_view()),
        path('tools/<int:pk>/', ToolsDetail.as_view())
    ]
    
    5. config/urls.py
    
    urlpatterns = [
        ...,
        path("api/", include("tools.urls"))
    ]

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django basic
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # django-rest-framework
+    'rest_framework',
+    # my apps
     'tools',
 ]
 
--- tools/views.py
+++ tools/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Tools
+from .serializers import ToolsSerializer
+
 
 # Create your views here.
+
+class ToolList(generics.ListCreateAPIView):
+    queryset = Tools.objects.all()
+    serializer_class = ToolsSerializer
+
+
+class ToolsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tools.objects.all()
+    serializer_class = ToolsSerializer
