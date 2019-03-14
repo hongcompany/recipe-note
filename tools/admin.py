d32c3cbc886414970019cec8b71d0adb4d53ff11
gurumee92 <32067275+gurumee92@users.noreply.github.com>
2019-03-14 23:59:28 +0900

    feature/TOOL_API #2 make model and admin apply
    1. models.py Tool(name) -> Tools(name (unique)) 변경
    2. admin.py 적용
    
    class ToolsAdmin(admin.ModelAdmin):
        list_display = ['id', 'name']
        list_editable = ['name']
    
    admin.site.register(Tools, ToolsAdmin)
    
    ====배운 명령어
    
    //db migrate
    python manage.py makemigrations tools
    python manage.py migrate tools

--- tools/admin.py
+++ tools/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tools
+
+
+class ToolsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_editable = ['name']
+
+
+admin.site.register(Tools, ToolsAdmin)
--- tools/models.py
+++ tools/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 
-class Tool(models.Model):
-    name = models.CharField(max_length=200)
+class Tools(models.Model):
+    name = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         return self.name
