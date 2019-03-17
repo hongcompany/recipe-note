03d8ea5384ae735721f7b1963fd62be995a4dda7
gurumee92 <32067275+gurumee92@users.noreply.github.com>
2019-03-17 23:32:32 +0900

    feature/INGREDIENTS_API #3 관리자 페이지 등록하기
    1. IngredientsAdmin 생성
    
    - ingredients/admin.py
    
        from django.contrib import admin
        from .models import Ingredients
    
        class IngredientsAdmin(admin.ModelAdmin):
            list_display = ['id', 'name']
            list_editable = ['name']
    
        admin.site.register(Ingredients, IngredientsAdmin)

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ingredients
+
+
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_editable = ['name']
+
+
+admin.site.register(Ingredients, IngredientsAdmin)
