7045cfe3d72a60407b92ea2f596c9d456a5472e8
홍준호 <brenden0730@gmail.com>
2019-03-27 14:59:32 +0900

    [Fix Disallowed host at jdango] - setting.py 안에 ALLOWED_HOSTS에 '0.0.0.0' 추가

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c8mcgdubo%6t2r0i1urmrr9ie0&22up38+czhcrrx75gf#2%93'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
